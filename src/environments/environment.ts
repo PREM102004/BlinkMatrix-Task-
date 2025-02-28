fd82141ff3251fcbb27c4fd97045e853aa6416f0
Prem Kumar <premkumar.s@blindmatrix.com>
2025-02-28 14:44:16 +0530

    fixes

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyCxfvx6EL67uYplMWrPZw2xXXu6CBsOUmI",
+      authDomain: "gocart-f7002.firebaseapp.com",
+      projectId: "gocart-f7002",
+      storageBucket: "gocart-f7002.appspot.com",
+      messagingSenderId: "20107549792",
+      appId: "1:20107549792:web:5beb063ad56bd2d1fd3705",
+      measurementId: "G-448T296XBM"
+    }
+  };
