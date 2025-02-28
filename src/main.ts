508686ffc4f926e25acbb7094e597b8f8c2a5687
Prem Kumar <premkumar.s@blindmatrix.com>
2025-02-28 15:32:47 +0530

    asas

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+// import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+// import { environment } from './environments/environment';
+// import { provideAuth, getAuth } from '@angular/fire/auth';
+// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+// import { provideStorage, getStorage } from '@angular/fire/storage';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { environment } from './environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideStorage, getStorage } from '@angular/fire/storage';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // ✅ No importProvidersFrom needed
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
-  ]
-}).catch(err => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // ✅ No importProvidersFrom needed
+//     provideAuth(() => getAuth()),
+//     provideFirestore(() => getFirestore()),
+//     provideStorage(() => getStorage())
+//   ]
+// }).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
